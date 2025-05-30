eb63a0b8211e4e46f7f4f3df81e10f91f26552b3
3-126-10-PC <you@example.com>
2025-05-31 11:30:44 +1200

    31.05.2025

--- Мессенджер/Js/script.js
+++ Мессенджер/Js/script.js
@@ -1,8 +1,8 @@
 //#region Глобальные переменные
 const HOST = 'http://api-messenger.web-srv.local'
 const CONTENT = _elem('.content')
-const INTERVAL_UPDATE_CHATS = 200;
-const INTERVAL_UPDATE_MSG = 200;
+const INTERVAL_UPDATE_CHATS = 20000;
+const INTERVAL_UPDATE_MSG = 20000;
 var CURRENT_CHAT;
 var GLOBAL_TIMERS = [];
 var TIMER_UPDATE_MSG;
@@ -83,6 +83,7 @@ function first() {
     _get({ url: '/Modules/AUTH.html' }, function (responseText) {
         CONTENT.innerHTML = responseText;
         auth();
+        clearInterval(GLOBAL_TIMERS);
         _elem(".btn-reg").addEventListener('click', function () {
             _load('/Modules/REG.html', function (responseText) {
                 CONTENT.innerHTML = responseText;
@@ -126,6 +127,7 @@ function registration() {
                     _load('/Modules/WORK.html', function (responseText) {
                         CONTENT.innerHTML = responseText;
                         document.querySelector('.user-name').textContent = regData.Data.fam + " " + regData.Data.name;
+                        // document.querySelector('.user-img').src
                         getChats();
                         createChat();
                         userInfo();
@@ -155,7 +157,7 @@ function auth() {
         HTTP_REQUEST.onreadystatechange = function () {
             if (HTTP_REQUEST.readyState == 4) {
                 AuthData = JSON.parse(HTTP_REQUEST.responseText);
-                //console.log(AuthData);
+                // console.log(AuthData);
 
                 if (AuthData.message) {
                     token = AuthData.Data.token;
@@ -165,11 +167,16 @@ function auth() {
                     //console.log(token);
                     _load('/Modules/WORK.html', function (responseText) {
                         CONTENT.innerHTML = responseText;
+                        _elem('.user-block .user-img').src = `${HOST}${AuthData.Data.photo_link}`;
                         _elem('.user-name').textContent = AuthData.Data.fam + " " + AuthData.Data.name;
-
+                        // img = _elem('.user-img')
+                        // img.src = AuthData.Data.photo_link
+                        // const container = document.getElementById('user-img')
+                        // container.append(img)
                         getChats();
                         createChat();
                         userInfo();
+
                     })
                 }
             }
@@ -185,23 +192,26 @@ function getChats() {
     HTTP_REQUEST.open('GET', `${HOST}/chats/`);
     HTTP_REQUEST.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('_token'));
     HTTP_REQUEST.send(chdata);
-    HTTP_REQUEST.onreadystatechange = function(){
-        if(HTTP_REQUEST.readyState == 4){
+    HTTP_REQUEST.onreadystatechange = function () {
+        if (HTTP_REQUEST.readyState == 4) {
             localStorage.getItem('_token');
             localStorage.getItem('_UserID')
             Chatdata = JSON.parse(HTTP_REQUEST.responseText);
+            GLOBAL_TIMERS.push(setInterval(getChats, INTERVAL_UPDATE_CHATS))
             //console.log(Chatdata);
             Chatdata.forEach(element => {
                 let block_chats = document.getElementById(`chat_${element.chat_id}`)
-                if(!block_chats){
+                if (!block_chats) {
                     _elem('.block_chats').append(
-                    createChatBlock(element)
-                )
+                        createChatBlock(element)
+                    )
 
-                }else{
-            
+                } else {
+                    if (block_chats.getAttribute('last-msg') != element.chat_last_message) {
+                        block_chats.style = 'background-color:red;'
+                    }
                 }
-                  postMess()  
+                postMess()
             });
 
         }
@@ -212,24 +222,24 @@ function getChats() {
 
 //#region Создание чатов
 
-function createChat(){
-    _elem('.create-chat').addEventListener('click', function(){
-            let crdata = new FormData();
-            crdata.append('email', _elem('input[name="email-work"]').value);
-            let HTTP_REQUEST = new XMLHttpRequest();
-            HTTP_REQUEST.open('POST', `${HOST}/chats/`);
-            HTTP_REQUEST.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('_token'));
-            HTTP_REQUEST.send(crdata);
-            HTTP_REQUEST.onreadystatechange = function(){
-                if(HTTP_REQUEST.readyState == 4){
-                    localStorage.getItem('_token');
-                    localStorage.getItem('_UserID');
-                    CreateData = JSON.parse(HTTP_REQUEST.responseText);
-                    //console.log(CreateData);
-                    getChats()
-                    _elem('.block_chats').append(getChats())
-                }
-    }       
+function createChat() {
+    _elem('.create-chat').addEventListener('click', function () {
+        let crdata = new FormData();
+        crdata.append('email', _elem('input[name="email-work"]').value);
+        let HTTP_REQUEST = new XMLHttpRequest();
+        HTTP_REQUEST.open('POST', `${HOST}/chats/`);
+        HTTP_REQUEST.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('_token'));
+        HTTP_REQUEST.send(crdata);
+        HTTP_REQUEST.onreadystatechange = function () {
+            if (HTTP_REQUEST.readyState == 4) {
+                localStorage.getItem('_token');
+                localStorage.getItem('_UserID');
+                CreateData = JSON.parse(HTTP_REQUEST.responseText);
+                //console.log(CreateData);
+                getChats()
+                _elem('.block_chats').append(getChats())
+            }
+        }
         // _post({url:`${HOST}/chats/`, Data: crdata}, function(responseText){
         //     responseText = JSON.parse(responseText);
         //     //console.log(responseText);
@@ -244,7 +254,7 @@ function createChat(){
 
 function createChatBlock(chatdata) {
     //console.log(chatdata);
-    
+
     let chatBlock = document.createElement('div');
     chatBlock.classList.add('block_chat');
     chatBlock.id = `chat_${chatdata.chat_id}`;
@@ -254,17 +264,18 @@ function createChatBlock(chatdata) {
     let chatName = document.createElement('p');
     chatName.textContent = chatdata.chat_name;
     chatBlock.append(chatName);
+    chatBlock.setAttribute('last-msg', chatdata.chat_last_message)
     // chatBlock.onclick = function () {
     //     CURRENT_CHAT = chatdata.chat_id;
     //     getMessage(chatdata.chat_id);
     //     // //console.log(CURRENT_CHAT);
-       
+
     //     // makeMess(chatdata)
-        
+
     //     // makeMess(chatdata);
-        
+
     // }
-        chatBlock.onclick = function () {
+    chatBlock.onclick = function () {
         CURRENT_CHAT = chatdata.chat_id;
         localStorage.setItem('_chatID', CURRENT_CHAT)
 
@@ -276,7 +287,7 @@ function createChatBlock(chatdata) {
         } else {
             _elem('.block_chat').classList.remove('active')
         }
-        
+
         this.classList.add('active')
 
         clearInterval(TIMER_UPDATE_MSG)
@@ -300,16 +311,16 @@ function createChatBlock(chatdata) {
 
 //#region Получение данных о пользователе
 
-function userInfo(){
-    _elem('.fa-bars').onclick = function(){
+function userInfo() {
+    _elem('.fa-bars').onclick = function () {
         _elem('.user-block').classList.toggle('hidden');
         loadInfo();
         closeInfo();
     }
 }
 
-function closeInfo(){
-    _elem('.fa-xmark').onclick = function(){
+function closeInfo() {
+    _elem('.fa-xmark').onclick = function () {
         _elem('.user-block').classList.add('hidden');
         userInfo();
     }
@@ -319,23 +330,23 @@ function closeInfo(){
 
 //#region Загрузка данных о пользователе
 
-function loadInfo(){
+function loadInfo() {
     let udata = new FormData();
     let HTTP_REQUEST = new XMLHttpRequest();
     HTTP_REQUEST.open('GET', `${HOST}/user/`);
     HTTP_REQUEST.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('_token'));
     HTTP_REQUEST.send(udata);
-    HTTP_REQUEST.onreadystatechange = function(){
-        if(HTTP_REQUEST.readyState == 4){
+    HTTP_REQUEST.onreadystatechange = function () {
+        if (HTTP_REQUEST.readyState == 4) {
             localStorage.getItem('_token');
             localStorage.getItem('UserID')
             userData = JSON.parse(HTTP_REQUEST.responseText);
             //console.log(userData);
-             _elem('.user-block .user-img').src = `${HOST}${userData.photo_link}`;
-             _elem('.user-block .user-email').textContent = userData.email;
-             _elem('.user-block .user-fam').textContent = userData.fam;
-             _elem('.user-block .user-name-work').textContent = userData.name;
-             exit();
+            _elem('.user-block .user-img').src = `${HOST}${userData.photo_link}`;
+            _elem('.user-block .user-email').textContent = userData.email;
+            _elem('.user-block .user-fam').textContent = userData.fam;
+            _elem('.user-block .user-name-work').textContent = userData.name;
+            exit();
         }
     }
 }
@@ -343,19 +354,20 @@ function loadInfo(){
 
 //#region Выход с учетной записи
 
-function exit(){
-    _elem('.footer-exit-text').onclick = function(){
+function exit() {
+    _elem('.footer-exit').onclick = function () {
         let exdata = new FormData();
         let HTTP_REQUEST = new XMLHttpRequest();
         HTTP_REQUEST.open('DELETE', `${HOST}/auth/`);
         HTTP_REQUEST.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('_token'));
         HTTP_REQUEST.send(exdata);
-        HTTP_REQUEST.onreadystatechange = function(){
-            if(HTTP_REQUEST.readyState == 4){
+        HTTP_REQUEST.onreadystatechange = function () {
+            if (HTTP_REQUEST.readyState == 4) {
                 localStorage.removeItem('_token');
                 localStorage.removeItem('_UserID');
                 localStorage.removeItem('_chatID');
                 first();
+
             }
         }
     }
@@ -365,29 +377,31 @@ function exit(){
 
 //#region Загрузка сообщений в чат
 
-function getMessage(chat_id){
+function getMessage(chat_id) {
     let mdata = new FormData();
     let HTTP_REQUEST = new XMLHttpRequest();
-    HTTP_REQUEST.open('GET', `${HOST}/messages/?chat_id=${chat_id}` );
+    HTTP_REQUEST.open('GET', `${HOST}/messages/?chat_id=${chat_id}`);
     HTTP_REQUEST.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('_token'));
     HTTP_REQUEST.send(mdata);
-    HTTP_REQUEST.onreadystatechange = function(){
-        if(HTTP_REQUEST.readyState == 4){
+    HTTP_REQUEST.onreadystatechange = function () {
+        if (HTTP_REQUEST.readyState == 4) {
             localStorage.getItem('_token');
             localStorage.getItem('_UserID');
             localStorage.getItem('_chatID');
             messdata = JSON.parse(HTTP_REQUEST.responseText);
             console.log(messdata);
-            
-            // CURRENT_CHAT = messdata.chat_id;
+
+            // CURRENT_CHAT = messdata.chat_id
+
             messdata.forEach(element => {
+
                 console.log(element);
-                
+
                 let message_block = document.getElementById(`msg_${element.id}`);
-                if(!message_block){
+                if (!message_block) {
                     let msgBlock = makeMess(element)
                     console.log(msgBlock);
-                    
+
                     _elem('.message_block').append(msgBlock)
                     // postMess();
                     // _elem('.msg-me').classList.add('hidden');
@@ -396,8 +410,9 @@ function getMessage(chat_id){
                     // _elem('.msg-my').classList.toggle('hidden');
                     // _elem('.msg-me').classList.toggle('hidden');
                     //console.log('messdata');   
-                    }
                 }
+
+            }
             )
             // //console.log(messdata);
         }
@@ -408,11 +423,11 @@ function getMessage(chat_id){
 
 //#region Создаие блока для сообщения
 
-function makeMess(mess){
+function makeMess(mess) {
 
     let msgBlock = document.createElement('div');
     msgBlock.classList.add('msg');
-    mess.sender.id == localStorage.getItem('_UserID') ? msgBlock.classList.add('msg-my'): msgBlock.classList.add('msg-me');
+    mess.sender.id == localStorage.getItem('_UserID') ? msgBlock.classList.add('msg-my') : msgBlock.classList.add('msg-me');
     msgBlock.id = `msg_${mess.id}`;
 
     let msgSender = document.createElement('h4');
@@ -430,7 +445,7 @@ function makeMess(mess){
 
     //console.log(mess);
     //console.log('makeMess()');
-    
+
     return msgBlock;
 
 }
@@ -439,10 +454,10 @@ function makeMess(mess){
 
 //#region Отправка сообщения в чат
 
-function postMess(){
+function postMess() {
 
-    _elem('.btn-post').addEventListener('click', function(){
-        
+    _elem('.btn-post').addEventListener('click', function () {
+
         let post = new FormData();
         post.append('chat_id', localStorage.getItem('_chatID'));
         post.append('text', _elem('input[name="message"]').value);
@@ -450,20 +465,16 @@ function postMess(){
         HTTP_REQUEST.open('POST', `${HOST}/messages/`);
         HTTP_REQUEST.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('_token'));
         HTTP_REQUEST.send(post);
-        HTTP_REQUEST.onreadystatechange = function(){
-            if(HTTP_REQUEST.readyState == 4){
+        HTTP_REQUEST.onreadystatechange = function () {
+            if (HTTP_REQUEST.readyState == 4) {
                 postData = JSON.parse(HTTP_REQUEST.responseText);
                 localStorage.getItem('_token');
                 localStorage.getItem('_UserID');
                 //console.log(postData);
                 //console.log(CURRENT_CHAT);
-                getMessage(postData);
-                makeMess(postData);
-                let nich = ''
-                _elem('input[name="message"]').value = nich
-                }
             }
         }
+    }
     )
 }
 
